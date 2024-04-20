f0370fff0fee18d9152c4c71e443e14f076a2265
nehakumari17 <nehak772903@gmail.com>
2024-04-20 13:33:24 +0530

    add

--- backend/controller/messageContoller.js
+++ backend/controller/messageContoller.js
@@ -1,10 +1,47 @@
-const sendMessage = (req, res) => {
+const conversationModel = require("../models/conversationModel")
+const messageModel = require("../models/messageModel")
+
+const sendMessage = async (req, res) => {
     try {
-        const senderId = req.id
+        const senderId = req.params.id
         const receiverId = req.params.id
         const {message} = req.body
+        let getConversation = await conversationModel.findOne({
+            participants: {$all: [senderId, receiverId]}
+        })
+
+        if (!getConversation) {
+            getConversation = await conversationModel.create({
+                participants: [senderId, receiverId]
+            })
+        }
+
+        const newMessage = await messageModel.create({
+            senderId,
+            receiverId,
+            message
+        })
+
+        if (newMessage) {
+            getConversation.messages.push(newMessage._id)
+        }
+        await getConversation.save()
+
+        return res.status(200).send({message: "Message send successfully!!"})
+
     } catch (error) {
         console.log("Error in sending messages..", error)
+        res.status(400).send({message: "Error in sending messages"})
+    }
+}
+
+const getMessage = async (req, res) => {
+    try{
+        const receiverId = req.params.id
+        const conversation = await conversationModel.findOne({})
+    } catch(error) {
+        console.log("Error in getting messages..", error)
+        res.status(400).send({message: "Error in getting messages"})
     }
 }
 
--- backend/routes/messageRoute.js
+++ backend/routes/messageRoute.js
@@ -1,6 +1,7 @@
 const router = require("express").Router()
 const { sendMessage } = require("../controller/messageContoller")
+const isAuthenticated = require("../middleware/authMiddleware")
 
-router.route("/send/:id").post(sendMessage)
+router.route("/send/:id").post(isAuthenticated, sendMessage)
 
 module.exports = router
